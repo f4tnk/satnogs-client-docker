72d6882741845930b5a45cd5ba602327d87a27cf
f4tnk <buzzbin@pm.me>
2024-10-19 22:52:49 +0200

    Fix satdump.sh

--- addons/scripts/satdump.sh
+++ addons/scripts/satdump.sh
@@ -15,11 +15,12 @@ PRG="SatDump:"
 : "${SATNOGS_APP_PATH:=/tmp/.satnogs}"
 : "${SATNOGS_OUTPUT_PATH:=/tmp/.satnogs/data/}"
 : "${UDP_DUMP_PORT:=57356}"
-: "${SATDUMP_KEEPLOGS:=no}"
+: "${SATDUMP_KEEPLOGS:=yes}"
 BIN=$(command -v satdump)
-LOG="SATNOGS_APP_PATH/satdump_$ID.log"
-OUT="SATNOGS_APP_PATH/satdump_$ID"
-PID="SATNOGS_APP_PATH/satdump_$SATNOGS_STATION_ID.pid"
+LOG="$SATNOGS_APP_PATH/satdump_$ID.log"
+OUT="$SATNOGS_APP_PATH/satdump_$ID"
+PID="$SATNOGS_APP_PATH/satdump_$SATNOGS_STATION_ID.pid"
+UNIXTD="${3:-$(date -u +%s)}" # date -d "2024-04-25T14:07:37" -u +%s
 
 SATNAME=$(echo "$TLE" | jq .tle0 | sed -e 's/ /_/g' | sed -e 's/[^A-Za-z0-9._-]//g')
 NORAD=$(echo "$TLE" | jq .tle2 | awk '{print $2}')
@@ -35,20 +36,31 @@ if [ "${CMD^^}" == "START" ]; then
   fi
   OPT=""
   case "$NORAD" in
-    "25338") # NOAA 15
-      OPT="live noaa_apt $OUT --source udp_source --port $UDP_DUMP_PORT --satellite_number 15 --samplerate $SAMP"
+      "25338" | "28654" | "33591") # NOAA 15 # NOAA 18 # NOAA 19
+          case "$SATNAME" in 
+            *"15"*)  satellite_number="15"
+            ;;
+            *"18"*)  satellite_number="18"
+            ;;
+            *"19"*)  satellite_number="19"
+            ;;
+            *) echo "Satdump : noaa satellite_number not found"
+          esac
+          OPT="live noaa_apt baseband $OBS $OUT --start_timestamp $UNIXTD --samplerate $SAMP --baseband_format cs16 --satellite_number $satellite_number"
       ;;
-    "28654") # NOAA 18
-      OPT="live noaa_apt $OUT --source udp_source --port $UDP_DUMP_PORT --satellite_number 18 --samplerate $SAMP"
+      "44387" | "57166" | "59051" | "40069") # METEOR-M N2-2 # METEOR-M N2-3 # METEOR-M N2-4 # METEOR-M N2
+          OPT="live meteor_m2_lrpt baseband $OBS $OUT --start_timestamp $UNIXTD --samplerate $SAMP --baseband_format cs16"
       ;;
-    "33591") # NOAA 19
-      OPT="live noaa_apt $OUT --source udp_source --port $UDP_DUMP_PORT --satellite_number 19 --samplerate $SAMP"
+      "38771" | "43689") # METOP-B AHRPT (1701.3MHz)
+          OPT="live metop_ahrpt baseband $OBS $OUT --start_timestamp $UNIXTD --samplerate $SAMP --baseband_format cs16"
       ;;
+      *) echo "Satdump : satellite not found"
   esac
 
   if [ -n "$OPT" ]; then
     mkdir -p "$OUT"
     echo "$PRG running at $SAMP sps on $SATNAME"
+    echo "$OPT"
     $BIN $OPT > "$LOG" 2>> "$LOG" &
     echo $! > "$PID"
   fi
@@ -64,14 +76,28 @@ if [ "${CMD^^}" == "STOP" ]; then
   if [ -s "$OUT" ]; then
     echo "$PRG processing data to network"
     # find images, rename/move to ${SATNOGS_OUTPUT_PATH}/data_<obsid>_YYYY-MM-DDTHH-MM-SS.png
-    ls "$OUT"
+    # data_obs/2024/10/19/17/10422188/data_10422188_2024-10-19T17-27-17.png
+    date_obs=date +"%Y-%d-%mT%H-%M-%C"
+
+    sleep 1
+    for file in $OUT/*.png; do
+      year=date +"%Y"
+      month=date +"%m"
+      day=date "+%d"
+      file_dest="${SATNOGS_OUTPUT_PATH}/${year}/${month}/${day}/data_$date_obs.png"
+      mv $file $file_dest
+      echo "Satdump : Update image $file_dest successful to satnogs"
+    done
+
     if [ ! "${SATDUMP_KEEPLOGS^^}" == "YES" ]; then
       rm -rf "$OUT"
     fi
+
   fi
 
   if [ ! -s "$LOG" ] || [ ! "${SATDUMP_KEEPLOGS^^}" == "YES" ]; then
-    rm -f "$LOG"
+    # rm -f "$LOG"
+    echo $LOG
   else
     echo "$PRG Keeping logs, you need to purge them manually."
   fi
