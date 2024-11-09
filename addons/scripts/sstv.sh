cae6791c07b9b6585ce07b7fe33f849334d53e9b
f4tnk <buzzbin@pm.me>
2024-11-09 23:10:13 +0100

    Fix sstv + info dev docker-compose.yml & station.env

--- addons/scripts/sstv.sh
+++ addons/scripts/sstv.sh
@@ -10,18 +10,14 @@ BAUD="$6"    # $6 baudrate
 SCRIPT="$7"  # $7 script name, satnogs_bpsk.py
 MODE="$8"    # $8 mode FM, FSK
 
-PRG="SSTV:"
+PRG="[sstv]"
 : "${SATNOGS_APP_PATH:=/tmp/.satnogs}"
 : "${SATNOGS_OUTPUT_PATH:=/tmp/.satnogs/data}"
-: "${UDP_DUMP_PORT:=57356}"
-: "${SATDUMP_KEEPLOGS:=yes}"
 : "${SATNOGS_REMOVE_OGG_FILES=true}"
 
 BIN=$(command -v sstv)
 LOG="$SATNOGS_APP_PATH/sstv_$ID.log"
 OUT="$SATNOGS_APP_PATH/sstv_$ID"
-PID="$SATNOGS_APP_PATH/sstv_$SATNOGS_STATION_ID.pid"
-UNIXTD="${3:-$(date -u +%s)}" # date -d "2024-04-25T14:07:37" -u +%s
 
 
 SATNAME=$(echo "$TLE" | jq .tle0 | sed -e 's/ /_/g' | sed -e 's/[^A-Za-z0-9._-]//g')
@@ -35,10 +31,11 @@ fi
 if [ "${CMD^^}" = "START" ]; then
   if [[ "${MODE,,}" =~ "sstv" ]]; then
     DATE_OBS=$(date +"%Y-%m-%dT%H-%M-%S")
-    OGG=$(find $SATNOGS_APP_PATH -type f -name "satnogs_${ID}_*.ogg")
-    OPT="-d \"$OGG\" -o \"$SATNOGS_APP_PATH/sstv_$ID.png\""
-    echo "$PRG $OPT"
-    $BIN $OPT
+
+    OGG=$(find "${SATNOGS_APP_PATH}" -type f \( -iname "satnogs_${ID}_*.ogg" \) -print -quit)
+
+    #Add "tty: true" in docker compose yaml
+    $BIN -d "${OGG}" -o "${SATNOGS_APP_PATH}/sstv_${ID}.png"
 
     if [ -f "$SATNOGS_APP_PATH/sstv_$ID.png" ]; then
       echo "$PRG Processing data $OUT to network"
