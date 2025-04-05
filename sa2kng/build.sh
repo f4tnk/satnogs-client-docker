3b1619c9e043bbd7bd2db7247d3529adafdc2666
f4tnk <buzzbin@pm.me>
2025-04-05 20:29:18 +0200

    Delete SATYAML File

--- sa2kng/build.sh
+++ sa2kng/build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
 
-REPO_ROOT="kerel64"
+REPO_ROOT="f4tnk"
 IMAGE_NAME="satnogs-client"
-IMAGE_TAG="kerel"
+IMAGE_TAG="gr310"
 
-ARGS="  --build-arg GNURADIO_IMAGE_TAG=3.10.5.1-satnogs"
+ARGS="  --build-arg GNURADIO_IMAGE_TAG=3.10.6-satnogs-f4tnk"
 
-ARGS+=" --build-arg GRSATNOGS_URL=https://gitlab.com/kerel-fs/gr-satnogs.git"
-ARGS+=" --build-arg GRSATNOGS_BRANCH=doppler"
-ARGS+=" --build-arg GRSATNOGS_VER=3.0.1.0~kerel20250311.43720e-1"
+#ARGS+=" --build-arg GRSATNOGS_URL=https://gitlab.com/kerel-fs/gr-satnogs.git"
+#ARGS+=" --build-arg GRSATNOGS_BRANCH=doppler"
+#ARGS+=" --build-arg GRSATNOGS_VER=3.0.1.0~kerel20250311.43720e-1"
 
 ARGS+=" --build-arg FLOWGRAPHS_URL=https://gitlab.com/kerel-fs/satnogs-flowgraphs.git"
 ARGS+=" --build-arg FLOWGRAPHS_BRANCH=fix_doppler"
 ARGS+=" --build-arg FLOWGRAPHS_VER=1.6~kerel20250311.140979-1"
 
-ARGS+=" --build-arg CLIENT_URL=https://gitlab.com/kerel-fs/satnogs-client.git"
+ARGS+=" --build-arg CLIENT_URL=https://gitlab.com/librespacefoundation/satnogs/satnogs-client"
 ARGS+=" --build-arg CLIENT_BRANCH=kerel"
 ARGS+=" --build-arg CLIENT_VER=1.9.4~kerel20250311.3fc21ec-1"
 
