9ec71965b6d45c0eb50ddc947af08a6064494ecc
f4tnk <buzzbin@pm.me>
2025-04-06 00:48:53 +0200

    Docker OK

--- addons/scripts/liveupdate-satyaml.sh
+++ addons/scripts/liveupdate-satyaml.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 cd || exit
 git clone -b f4tnk --depth 1 https://github.com/f4tnk/satnogs-client-docker
-cp satnogs-client-docker/addons/satyaml/* /usr/local/lib/python3.11/dist-packages/satellites/satyaml
-find satnogs-client-docker/addons/satyaml/* -type f -name "*.yml" -exec cp {} /usr/local/lib/python3.11/dist-packages/satellites/satyaml/ \;
+find satnogs-client-docker/addons/satyaml/* -type f -name "*.yml" -exec cp {} /usr/lib/python3/dist-packages/satellites/satyaml/ \;
 rm -rf satnogs-client-docker
 
 git clone -b master --depth 1 https://github.com/daniestevez/gr-satellites.git
-find gr-satellites/python/satyaml/* -type f -name "*.yml" -exec cp {} /usr/local/lib/python3.11/dist-packages/satellites/satyaml/ \;
+find gr-satellites/python/satyaml/* -type f -name "*.yml" -exec cp {} /usr/lib/python3/dist-packages/satellites/satyaml/ \;
 rm -rf gr-satellites /tmp/.satnogs/grsat_list.*
 
 git clone --depth 1 https://github.com/janvgils/Launches
-find Launches -type f -name "*.yml" -exec cp {} /usr/local/lib/python3.11/dist-packages/satellites/satyaml/ \;
+find Launches -type f -name "*.yml" -exec cp {} /usr/lib/python3/dist-packages/satellites/satyaml/ \;
 rm -rf Launches
 
 exec "$@"
